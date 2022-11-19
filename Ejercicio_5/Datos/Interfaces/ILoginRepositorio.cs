0d7aea6dc40375595bc6a6252a49107cbab3794a
lindasierra <113157188+lindasierra@users.noreply.github.com>
2022-11-19 01:53:43 -0600

    Primer Commit

---
+++ Ejercicio_5/Datos/Interfaces/ILoginRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Interfaces
+{
+    public interface ILoginRepositorio
+    {
+
+        Task<bool> ValidarUsuario(Login login);
+
+
+
+
+
+    }
+}
---
+++ Ejercicio_5/Datos/Repositorio/LoginRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Datos.Interfaces;
+using Entidades;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorio
+{
+    public class LoginRepositorio : ILoginRepositorio
+    {
+        private string CadenaConexion;
+
+        public LoginRepositorio(string cadena_conexion)
+        {
+            CadenaConexion = cadena_conexion;
+        }
+
+        private MySqlConnection Conexion()
+        {
+            return new MySqlConnection(CadenaConexion);
+        }
+        public async Task<bool> ValidarUsuario(Login login)
+        {
+            bool valido = false;
+            try
+            {
+                using MySqlConnection conexion = Conexion();
+                await conexion.OpenAsync();
+                string sql = "SELECT 1 FROM Usuario WHERE = @Codigo AND Clave = @Clave;";
+                valido = await conexion.ExecuteScalarAsync<bool>(sql, new {login.Usuario, login.Clave});
+            }
+            catch (Exception)
+            {
+                
+            }
+            return valido;
+        }
+    }
+}
